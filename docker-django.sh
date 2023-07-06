afd7e6aabd122431fdc0187cdb8c431f38390971
Dan <github@lint.email>
2023-07-06 15:27:46 -0700

    Update docker-django.sh

--- docker-django.sh
+++ docker-django.sh
@@ -25,3 +25,10 @@ cd $webpath
 
 #
 python3 -m venv $webpath/venv
+
+source $webpath/venv/bin/activate
+
+python3 -m pip install django
+
+django-admin startproject dannylint
+python3 manage.py migrate
