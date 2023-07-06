12f0ba35b00ebb9ad1702fe33258084131a8a9f9
Dan <github@lint.email>
2023-07-06 15:38:47 -0700

    Update docker-django.sh

--- docker-django.sh
+++ docker-django.sh
@@ -10,6 +10,7 @@ apt-get install -y python3 python3-pip python3-venv
 python3 --version
 pip3 --version
 
+project_dir="dannylint"
 webpath="/home/web"
 
 # Check if the directory exists
@@ -23,7 +24,15 @@ fi
 
 cd $webpath
 
-# 
+# Check if the project directory exists
+if [ ! -d "$webpath/$project_dir" ]; then
+    # Execute the command
+    django-admin startproject "$project_dir"
+    echo "Project created."
+else
+    echo "Project directory already exists. Skipping command."
+fi
+
 python3 -m venv $webpath/venv
 
 source $webpath/venv/bin/activate
